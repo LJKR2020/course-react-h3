dbaaaa38c0d10a591db8dccab089c4b3c919bdc2
LJKR2020 <loredana.rietveld@gmail.com>
2021-11-24 12:44:37 +0100

    made form with required fields

--- src/App.js
+++ src/App.js
@@ -3,9 +3,47 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      Maak hier jouw formulier!
-    </div>
+      <>
+          <form>
+              <h3>Gegevens</h3>
+              <label htmlFor='firstName'> Naam:
+                  <input
+                      type='text'
+                      id='firstName'
+                      name='name'
+                  />
+              </label>
+              <label htmlFor='age'> Leeftijd:
+                  <input
+                      type='number'
+                      id='age'
+                      name='age'
+                  />
+              </label>
+              <h3>Jouw review</h3>
+              <label htmlFor='message'>
+                  Opmerkingen:
+                  <input
+                      type='text'
+                      id='message'
+                      name='message'
+                  />
+              </label>
+              <label htmlFor='newsletter'>
+                  <input
+                      type='checkbox'
+                      id='newsletter'
+                      name='newsletter'
+                  />
+                  Ik schrijf me in voor de nieuwsbrief
+              </label>
+              <label htmlFor="sendButton">
+                  <button type='button'>
+                      Versturen
+                  </button>
+              </label>
+          </form>
+      </>
   );
 }
 
