2de75ea323f6e9a311fe7d933cd891fb0d025f9e
LJKR2020 <loredana.rietveld@gmail.com>
2021-11-25 12:32:45 +0100

    added function handleSubmit and the preventdefault

--- src/App.js
+++ src/App.js
@@ -2,16 +2,37 @@ import React from 'react';
 import './App.css';
 
 function App() {
+    // const [formData, setFormData] = React.useState([
+    // al die andere.....
+    // ])
     const [nameValue, setNameValue] = React.useState('')
     const [ageValue, setAgeValue] = React.useState(0)
     const [messageValue, setMessageValue] = React.useState('')
     const [checkedNews, setCheckedNews] = React.useState(false)
+
+
+    function handleSubmit(e) {
+        e.preventDefault()
+
+        if (e.target.elements.length > 0) {
+            const formElements = Array.from(e.target.elements);
+            const fields = formElements.filter((field) => {
+                    return field.nodeName === "INPUT" || field.nodeName === "TEXTAREA"
+                })
+            console.log(fields)
+        //    setFormData()
+        }
+    }
+
     return (
         <>
             <div className='form-container'>
-                <fieldset className='form-styling'>
-                    <legend id='formTitle'>Gegevens</legend>
-                    <form>
+                <form
+                    onSubmit={handleSubmit}
+                >
+                    <fieldset className='form-styling'>
+                        <legend id='formTitle'>Gegevens</legend>
+
                         <label htmlFor='firstName'> Naam:
                             <input
                                 type='text'
@@ -22,8 +43,6 @@ function App() {
                                 onChange={(e) => setNameValue(e.target.value)}
                             />
                         </label>
-                    </form>
-                    <form>
                         <label htmlFor='age'> Leeftijd:
                             <input
                                 type='number'
@@ -33,18 +52,15 @@ function App() {
                                 onChange={(e) => setAgeValue(e.target.value)}
                             />
                         </label>
-                    </form>
-                </fieldset>
-                <fieldset className='form-styling'>
+                    </fieldset>
+                    <fieldset className='form-styling'>
                         <legend id='formTitle'>
                             Jouw review
                         </legend>
-                    <form>
                         <label htmlFor='message'>
                             Opmerkingen:
                         </label>
                         <textarea
-                            type='text'
                             id='message'
                             name='message'
                             placeholder='Wat vond je van het recept?'
@@ -52,8 +68,6 @@ function App() {
                             onChange={(e) => setMessageValue(e.target.value)}
                         >
                         </textarea>
-                    </form>
-                    <form>
                         <label htmlFor='newsletter'>
                             <input
                                 type='checkbox'
@@ -64,14 +78,14 @@ function App() {
                             />
                             Ik schrijf me in voor de nieuwsbrief
                         </label>
-                    </form>
-                    <button
-                        disabled={!checkedNews}
-                        type='submit'
-                    >
-                        Versturen
-                    </button>
-                </fieldset>
+                        <button
+                            disabled={!checkedNews}
+                            type='submit'
+                        >
+                            Versturen
+                        </button>
+                    </fieldset>
+                </form>
             </div>
         </>
     );
